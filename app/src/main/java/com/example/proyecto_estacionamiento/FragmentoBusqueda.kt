1513987519caf75980634ed2d3e5255d610e6b09
JoaquinIMT <A01740395@itesm.mx>
2019-05-03 16:36:37 -0600

    Ya tenemos un recycler view trabajando en el frame de buscar automoviles

--- app/src/main/java/com/example/proyecto_estacionamiento/FragmentoBusqueda.kt
+++ app/src/main/java/com/example/proyecto_estacionamiento/FragmentoBusqueda.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_busqueda.*
 import kotlinx.android.synthetic.main.fragment_primer_fragmento.*
 
 class FragmentoBusqueda : Fragment() {
@@ -14,11 +16,14 @@ class FragmentoBusqueda : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_busqueda, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerview_carros.layoutManager = LinearLayoutManager(context?.applicationContext)
+        recyclerview_carros.adapter = MainAdapter()
 
     }
 
--- app/src/main/java/com/example/proyecto_estacionamiento/MainAdapter.kt
+++ app/src/main/java/com/example/proyecto_estacionamiento/MainAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.proyecto_estacionamiento
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,21 +8,24 @@ import java.lang.reflect.Array
 
 
 //Esta clase es para que los elementos se generen repetitivamente con datos especificos
-class MainAdapter(Carros: ArrayList<Array>): RecyclerView.Adapter<CustomViewHolder>() {
+class MainAdapter/*(Carros: ArrayList<Array>)*/: RecyclerView.Adapter<CustomViewHolder>() {
 
-    val carros = Carros
+    //val carros = Carros
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val lineOfRow = layoutInflater.inflate(R.layout.carros_row,parent,false)
+        return CustomViewHolder(lineOfRow)
     }
 
     override fun getItemCount(): Int {
 
-        return carros.size
+        return 10
+        //return carros.size
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 
