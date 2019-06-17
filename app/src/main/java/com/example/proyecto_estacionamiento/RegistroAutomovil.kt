16b38dab615fc87e8628352d65cc222e9188545d
eduardoLuq <luque2497@gmail.com>
2019-06-17 00:44:54 -0600

    autocompletar
    autocompleta marca y modelo del vehiculo

--- app/src/main/java/com/example/proyecto_estacionamiento/RegistroAutomovil.kt
+++ app/src/main/java/com/example/proyecto_estacionamiento/RegistroAutomovil.kt
@@ -17,6 +17,7 @@ class RegistroAutomovil(): AppCompatActivity() {
 
     lateinit var matricula: TextView
     lateinit var marcaAutoCompletar: AutoCompleteTextView
+    lateinit var modeloAutoCompletar: AutoCompleteTextView
     lateinit var marca: TextView
     lateinit var modelo: TextView
     lateinit var enHora: TextView
@@ -31,17 +32,42 @@ class RegistroAutomovil(): AppCompatActivity() {
     lateinit var registro : Button //Boton abajo de la pantalla para concluir cambios
     lateinit var hora : Date
     var entradaMili : Long? = null
-    var marcas = arrayOf("Audi","BMW","Chevrolet","Chrysler","Corvette","Dodge","Fiat","Ford","GMC","Honda","Hummer","Hyundai","Isuzu","Jaguar","Jeep","Kia","Land-rover","Mazda","Mercedes-benz","Mini",
-                         "Mitsubishi","Nissan","Pontiac","Porsche","Renault","Smart","Suzuki","Toyota","Volkswagen","Volvo")
-    var modelos = arrayOf("80,a4,a6,s6,coupe,s2,rs2,a8,cabriolet,s8,a3,s4,tt,s3,allroad-quattro,rs4,a2,rs6,q7,r8,a5,s5,v8,200,100,90,tts,q5,a4-allroad-quattro,tt-rs,rs5,al,a7,rs3,q3,a6-allroad-quattro,s7,sq5",
-                          "serie-3,serie-5,compact,serie-7,serie-8,z3,z4,z8,x5,serie-6,x3,serie-1,z1,x6,x1")
-
-
-
-
-
-
-
+    var marcas = arrayOf(
+                         "Audi","BMW","Chevrolet","Chrysler","Corvette","Dodge","Fiat","Ford","GMC","Honda","Hummer","Hyundai","Isuzu","Jaguar","Jeep","Kia","Land-rover","Mazda","Mercedes-benz","Mini",
+                         "Mitsubishi","Nissan","Pontiac","Porsche","Renault","Smart","Suzuki","Toyota","Volkswagen","Volvo"
+    )
+    var modelos = arrayOf(
+                          "80,a4,a6,s6,coupe,s2,rs2,a8,cabriolet,s8,a3,s4,tt,s3,allroad-quattro,rs4,a2,rs6,q7,r8,a5,s5,v8,200,100,90,tts,q5,a4-allroad-quattro,tt-rs,rs5,al,a7,rs3,q3,a6-allroad-quattro,s7,sq5",
+                          "serie-3,serie-5,compact,serie-7,serie-8,z3,z4,z8,x5,serie-6,x3,serie-1,z1,x6,x1",
+                          "Corvette,Blazer,Astro,Nubira,Evanda,Trans Sport,Camaro,Matiz,Alero,Tahoe,Tacuma,Trailblazer,Kalos,Aveo,Lacetti,Epica,Captiva,Hhr,Cruze,Spark,Orlando,Volt,Malibu",
+                          "Vision,300m,Grand Voyager,Viper,Neon,Voyager,Stratus,Sebring,Sebring 200c,New Yorker,Pt Cruiser,Crossfire,300c,Le Baron,Saratoga",
+                          "Corvette",
+                          "Viper,Caliber,Nitro,Avenger,Journey",
+                          "Croma,Cinquecento,Seicento,Punto,Grande Punto,Panda,Tipo,Coupe,Uno,Ulysse,Tempra,Marea,Barchetta,Bravo,Stilo,Brava,Palio Weekend,600,Multipla,Idea,Sedici,Linea,500,Fiorino,Ducato,Doblo Cargo,Doblo,Strada,Regata,Talento,Argenta,Ritmo,Punto Classic,Qubo,Punto Evo,500c,Freemont,Panda Classic,500l",
+                          "Maverick,Escort,Focus,Mondeo,Scorpio,Fiesta,Probe,Explorer,Galaxy,Ka,Puma,Cougar,Focus Cmax,Fusion,Streetka,Cmax,Smax,Transit,Courier,Ranger,Sierra,Orion,Pick Up,Capri,Granada,Kuga,Grand Cmax,Bmax,Tourneo Custom",
+                          "GMC",
+                          "Accord,Civic,Crx,Prelude,Nsx,Legend,Crv,Hrv,Logo,S2000,Stream,Jazz,Frv,Concerto,Insight,Crz",
+                          "H2,H3,H3t",
+                          "Lantra,Sonata,Elantra,Accent,Scoupe,Coupe,Atos,H1,Atos Prime,Xg,Trajet,Santa Fe,Terracan,Matrix,Getz,Tucson,I30,Pony,Grandeur,I10,I800,Sonata Fl,Ix55,I20,Ix35,Ix20,Genesis,I40,Veloster",
+                          "Trooper,Pick Up,D Max,Rodeo,Dmax,Trroper",
+                          "Serie Xj,Serie Xk,Xj,Stype,Xf,Xtype",
+                          "Wrangler,Cherokee,Grand Cherokee,Commander,Compass,Wrangler Unlimited,Patriot",
+                          "Sportage,Sephia,Sephia Ii,Pride,Clarus,Shuma,Carnival,Joice,Magentis,Carens,Rio,Cerato,Sorento,Opirus,Picanto,Ceed,Ceed Sporty Wagon,Proceed,K2500 Frontier,K2500,Soul,Venga,Optima,Ceed Sportswagon",
+                          "Range Rover,Defender,Discovery,Freelander,Range Rover Sport,Discovery 4,Range Rover Evoque",
+                          "Xedos 6,626,121,Xedos 9,323,Mx3,Rx7,Mx5,Mazda3,Mpv,Demio,Premacy,Tribute,Mazda6,Mazda2,Rx8,Mazda5,Cx7,Serie B,B2500,Bt50,Mx6,929,Cx5",
+                          "Clase C,Clase E,Clase Sl,Clase S,Clase Cl,Clase G,Clase Slk,Clase V,Viano,Clase Clk,Clase A,Clase M,Vaneo,Slklasse,Slr Mclaren,Clase Cls,Clase R,Clase Gl,Clase B,100d,140d,180d,Sprinter,Vito,Transporter,280,220,200,190,600,400,Clase Sl R129,300,500,420,260,230,Clase Clc,Clase Glk,Sls Amg",
+                          "Mini",
+                          "Montero,Galant,Colt,Space Wagon,Space Runner,Space Gear,3000 Gt,Carisma,Eclipse,Space Star,Montero Sport,Montero Io,Outlander,Lancer,Grandis,L200,Canter,300 Gt,Asx,Imiev",
+                          "Terrano Ii,Terrano,Micra,Sunny,Primera,Serena,Patrol,Maxima Qx,200 Sx,300 Zx,Patrol Gr,100 Nx,Almera,Pathfinder,Almera Tino,Xtrail,350z,Murano,Note,Qashqai,Tiida,Vanette,Trade,Vanette Cargo,Pickup,Navara,Cabstar E,Cabstar,Maxima,Camion,Prairie,Bluebird,Np300 Pick Up,Qashqai2,Pixo,Gtr,370z,Cube,Juke,Leaf,Evalia",
+                          "Trans Sport,Firebird,Trans Am",
+                          "911,Boxster,Cayenne,Carrera Gt,Cayman,928,968,944,924,Panamera,918",
+                          "Megane,Safrane,Laguna,Clio,Twingo,Nevada,Espace,Spider,Scenic,Grand Espace,Avantime,Vel Satis,Grand Scenic,Clio Campus,Modus,Express,Trafic,Master,Kangoo,Mascott,Master Propulsion,Maxity,R19,R25,R5,R21,R4,Alpine,Fuego,R18,R11,R9,R6,Grand Modus,Kangoo Combi,Koleos,Fluence,Wind,Latitude,Grand Kangoo Combi",
+                          "Smart,Citycoupe,Fortwo,Cabrio,Crossblade,Roadster,Forfour",
+                          "Maruti,Swift,Vitara,Baleno,Samurai,Alto,Wagon R,Jimny,Grand Vitara,Ignis,Liana,Grand Vitara Xl7,Sx4,Splash,Kizashi",
+                          "Carina E,4runner,Camry,Rav4,Celica,Supra,Paseo,Land Cruiser 80,Land Cruiser 100,Land Cruiser,Land Cruiser 90,Corolla,Auris,Avensis,Picnic,Yaris,Yaris Verso,Mr2,Previa,Prius,Avensis Verso,Corolla Verso,Corolla Sedan,Aygo,Hilux,Dyna,Land Cruiser 200,Verso,Iq,Urban Cruiser,Gt86",
+                          "Passat,Golf,Vento,Polo,Corrado,Sharan,Lupo,Bora,Jetta,New Beetle,Phaeton,Touareg,Touran,Multivan,Caddy,Golf Plus,Fox,Eos,Caravelle,Tiguan,Transporter,Lt,Taro,Crafter,California,Santana,Scirocco,Passat Cc,Amarok,Beetle,Up,Cc",
+                          "440,850,S70,V70,V70 Classic,940,480,460,960,S90,V90,Classic,S40,V40,V50,V70 Xc,Xc70,C70,S80,S60,Xc90,C30,780,760,740,240,360,340,Xc60,V60,V40 Cross Country"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +76,7 @@ class RegistroAutomovil(): AppCompatActivity() {
         matricula = this.findViewById(R.id.matricula2)
         marca = findViewById(R.id.matricula)
         marcaAutoCompletar= findViewById(R.id.matricula)
+        modeloAutoCompletar= findViewById(R.id.modelo)
         var adapterr : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,marcas)
         this.marcaAutoCompletar.setAdapter(adapterr)
         modelo = findViewById(R.id.modelo)
@@ -60,22 +87,25 @@ class RegistroAutomovil(): AppCompatActivity() {
         modelo.setOnClickListener{
             Toast.makeText(this@RegistroAutomovil, "estoy aqui", Toast.LENGTH_SHORT).show()
             if (marca.text.isEmpty()) {
-                Toast.makeText(this@RegistroAutomovil, "Soy nulo", Toast.LENGTH_SHORT).show()
+
             }
             else{
-                Toast.makeText(this@RegistroAutomovil, "Soy diferente de nulo", Toast.LENGTH_SHORT).show()
                 for(num in 0..marcas.size-1){
-                    Toast.makeText(this@RegistroAutomovil, num.toString(), Toast.LENGTH_SHORT).show()
-                    if(marcas.get(0).equals(marca.text)){
-                        posicion = num
-                        Toast.makeText(this@RegistroAutomovil, posicion, Toast.LENGTH_SHORT).show()
+                    var textoPosicion:String = marcas.get(num)
+                    var textoMarca:String = marca.text.toString()
+                    var comparacion = textoPosicion.compareTo(textoMarca)
+                    if(comparacion==0){
+                        var mod:String=modelos.get(num)
+                        val modelosCorre = mod.split(",")
+                        Toast.makeText(this@RegistroAutomovil, mod, Toast.LENGTH_SHORT).show()
+                        var adapterr : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,modelosCorre)
+                        this.modeloAutoCompletar.setAdapter(adapterr)
 
                     }
                 }
-
-                }
-
             }
+
+        }
 
 
         estacionamiento = intent.getParcelableExtra("Estacionamiento")
